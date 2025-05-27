9d7e1f38a6f9f182d9a1a0d78bc48239e18dc41e
Valter Igor <valter_igor@yahoo.com.br>
2025-05-26 22:15:54 -0300

    inclui e tirei arquivos

--- script.js
+++
@@ -1,8 +0,0 @@
-// Script para um efeito de fade-in para as imagens enquanto elas são carregadas
-window.addEventListener("load", function() {
-    const photos = document.querySelectorAll('.photo img');
-    photos.forEach((photo, index) => {
-        photo.style.animation = `fadeIn 1s ease ${index * 0.2}s forwards`;
-    });
-});
-
